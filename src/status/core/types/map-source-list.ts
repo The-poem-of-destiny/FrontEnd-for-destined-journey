246a52753ddce358b114b766e1ac03ae13f6271d
Hilo114 <suole2333@gmail.com>
2026-02-23 00:24:22 +0800

    fix

--- src/status/core/types/map-source-list.ts
+++ src/status/core/types/map-source-list.ts
@@ -10,11 +10,11 @@ export const mapSourceList: MapSourceConfig[] = [
   {
     key: 'small',
     name: '高清地图',
-    url: 'https://i.ibb.co/PL92LkT/Maplite.webp',
+    url: 'https://i.ibb.co/07F075B/Maplite.webp',
   },
   {
     key: 'large',
     name: '超清地图',
-    url: 'https://i.ibb.co/fzNWXMnq/Map.webp',
+    url: 'https://i.ibb.co/wFQqdywB/Map.webp',
   },
 ];
